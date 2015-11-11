1d495464a42cefa1062c8d1469e5907d8119fe79
Dan Robinson <danrobinson010@gmail.com>
2015-11-11 03:40:50 +0000

    added filter, mutate, select, and distinct

--- index.js
+++ index.js
@@ -1,17 +1,254 @@
-var DataFrame = function(data) {
-  if (data.length) {
-    this.setColumnNames(data[0]);
-    this.setColumnTypes(data[0]);
-    this.setColumns();
+var Promise = require("bluebird");
+var Rx = require("rx");
+
+var copySchema = function(schema, selectedNames, changedNames) {
+  var columnNames = schema.columnNames;
+  var columnTypes = schema.columnTypes;
+  var newColumnNames = [];
+  var newColumnTypes = [];
+  var changedNameKeys = (typeof changedNames === "object") ? Object.keys(changedNames) : undefined;
+  for (var i = 0; i < columnNames.length; i++) {
+    var columnName = columnNames[i];
+    if (changedNames && changedNameKeys.indexOf(columnName) > -1) {
+      newColumnNames.push(changedNames[columnName]);
+      newColumnTypes.push(columnTypes[i]);
+    } else if (!selectedNames || selectedNames.indexOf(columnName) > -1) {
+      newColumnNames.push(columnName);
+      newColumnTypes.push(columnTypes[i]);      
+    } 
   }
+  return {columnNames: newColumnNames, columnTypes: newColumnTypes};
+}
+
+var swap = function(obj) {
+  // swap keys and values
+
+  var swapped = {};
+  for (var key in obj) {
+    swapped[obj[key]] = key;
+  }
+  return swapped;
+}
+
+var DataFrame = function(data, options) {
+  options = options || {};
   var rowArray = [];
-  var row;
-  for (var i = 0; i < data.length; i++) {
-    row = new Row(this, data[i]);
-    rowArray.push(row);
+  if (data.length) {
+    var schema = options.schema || this.getSchema(data[0]);
+    if (!schema.hasOwnProperty("columnTypes")) {
+      schema.columnTypes = this.getColumnTypes(data[0], schema.columnNames);
+    }
+    this.schema = schema;
+    this.setColumns();
+
+    if (options.safe) {
+      rowArray = data;
+    } else {
+      for (var i = 0; i < data.length; i++) {
+        rowArray.push(new Row(data[i], schema));
+      }
+    }
   }
   this.rowArray = rowArray;
-  this._index = 0;
+  this.sequence = Rx.Observable.from(this.rowArray);
+}
+
+DataFrame.prototype.filter = function(func) {
+  this.sequence = this.sequence.filter(func);
+  return this;
+}
+
+DataFrame.prototype.mutate = function(newColumnName, func) {
+  var newSchema = undefined;
+  var newType = undefined;
+  this.sequence = this.sequence.map(function(row) {
+    var value = func(row);
+    var newRow = Object.create(row);
+    if (typeof newSchema === "undefined") {
+      newSchema = copySchema(row.schema);
+      newSchema.columnNames.push(newColumnName);
+      newType = typeof value;
+      newSchema.columnTypes.push(newType);
+    }
+    newRow.schema = newSchema;
+    newRow[newColumnName] = (typeof value === newType) ? value : null;
+    return newRow;
+  });
+  return this;
+}
+
+DataFrame.prototype.select = function() {
+  var newNames = [];
+  var namesToChange = {};
+  for (var i = 0; i < arguments.length; i++) {
+    var arg = arguments[i];
+    var argType = typeof arg;
+    if (argType === "string") {
+      newNames.push(arg);
+    } else if (argType === "object") {
+      for (var columnName in arg) {
+        namesToChange[columnName] = arg[columnName];
+      }
+    }
+  }
+  var newSchema = undefined;
+  this.sequence = this.sequence.map(function(row) {
+    if (typeof newSchema === "undefined") {
+      newSchema = copySchema(row.schema, newNames, namesToChange);
+    }
+    var changedNames = swap(namesToChange);
+    var newRow = new Row(row, newSchema, changedNames);
+    return newRow;
+  });
+  return this;
+}
+
+DataFrame.prototype.rename = function(namesToChange) {
+  if (typeof namesToChange !== "object") {
+    return this;
+  }
+  var newSchema = undefined;
+  this.sequence = this.sequence.map(function(row) {
+    if (typeof newSchema === "undefined") {
+      newSchema = copySchema(row.schema, undefined, namesToChange);
+    }
+    var changedNames = swap(namesToChange);
+    var newRow = new Row(row, newSchema, changedNames);
+    return newRow;
+  });
+  return this;
+}
+
+DataFrame.prototype.distinct = function() {
+  var distinctObjects = Object.create(null);
+  var distinctColumns = arguments.length > 0 ? [] : undefined;
+  for (var i = 0; i < arguments.length; i++) {
+    var arg = arguments[i];
+    var argType = typeof arg;
+    console.log(this.schema);
+    if (argType === "string") {
+      distinctColumns.push(arg);      
+    }
+  }
+  var filteredDistinctColumns = undefined;
+  this.sequence = this.sequence.filter(function(row) {
+    if (filteredDistinctColumns == undefined) {
+      filteredDistinctColumns = [];
+      for (var i = 0; i < distinctColumns.length; i++) {
+        var distinctColumnName = distinctColumns[i];
+        if (row.schema.columnNames.indexOf(distinctColumnName) > -1) {
+          filteredDistinctColumns.push(distinctColumnName);
+        }
+      }
+    }
+    if (filteredDistinctColumns.length == 0) {
+      return true;
+    }
+    var uniqueString = row.toUniqueString(filteredDistinctColumns);
+    console.log(uniqueString);
+    if (distinctObjects[uniqueString] !== undefined) {
+      return false;
+    } else {
+      distinctObjects[uniqueString] = true;
+      return true;
+    }
+  });
+  return this;
+}
+
+DataFrame.prototype.collect = function() {
+  var promiseArray = this.sequence.toArray().toPromise(Promise);
+  var promiseDF = promiseArray.then(function(newArray) {
+      var schema = newArray[0].schema;
+      var df = new DataFrame(newArray, {schema: schema, safe: true});
+      return df;
+    });
+  return promiseDF;
+}
+
+DataFrame.prototype.getSchema = function(rowData) {
+  var columnNames = Object.keys(rowData);  
+  var columnTypes = this.getColumnTypes(rowData, columnNames);
+  return {columnNames: columnNames, columnTypes: columnTypes};
+}
+
+DataFrame.prototype.getColumnTypes = function(rowData, columnNames) {
+  var columnTypes = [];
+  var name, value, valueType;
+  for (var i = 0; i < columnNames.length; i++) {
+    name = columnNames[i];
+    value = rowData[name];
+    valueType = typeof value;
+    columnTypes.push(valueType);
+  }
+  return columnTypes;
+}
+
+DataFrame.prototype["@@iterator"] = function() {
+  return new RowIterator(this);
+}
+
+DataFrame.prototype.setColumns = function() {
+  var columns = [];
+  var columnNames = this.schema.columnNames;
+  var columnTypes = this.schema.columnTypes;
+  var name, type;
+  for (var i = 0; i < columnNames.length; i++) {
+    name = columnNames[i];
+    type = columnTypes[i];
+    var column = new Column(this, name, type, i);
+    columns[name] = column;
+    columns.push(column);
+  }
+  this.columns = columns;
+}
+
+DataFrame.prototype.row = function(rowIndex) {
+  return this.rowArray[rowIndex];
+}
+
+DataFrame.prototype.cell = function(rowIndex, colIndex) {
+  var row = this.rowArray[rowIndex];
+  return typeof row !== "undefined" ? row.get(colIndex) : undefined;
+}
+
+DataFrame.prototype.slice = function(startIndex, endIndex) {
+  return new DataFrame(this.rowArray.slice(startIndex, endIndex), this.schema);
+}
+
+DataFrame.prototype.show = function() {
+  var columnNames = this.schema.columnNames;
+  console.log(columnNames.join(" "));
+  for (var rowIndex = 0; rowIndex < this.rowArray.length; rowIndex++) {
+    console.log(this.rowArray[rowIndex].toString());
+  }
+}
+
+function Column(df, name, type, index) {
+  this.df = df;
+  this.name = name;
+  this.type = type;
+  this.index = index;
+}
+
+Column.prototype.get = function(rowIndex) {
+  var row = this.df.row(rowIndex)
+  return row[this.name];
+}
+
+Column.prototype.toArray = function() {
+  var columnArray = [];
+  var columnIterator = this["@@iterator"]();
+  var next = columnIterator.next();
+  while (!next.done) {
+    columnArray.push(next.value);
+    next = columnIterator.next();
+  }
+  return columnArray;
+}
+
+Column.prototype["@@iterator"] = function() {
+  return new SingleColumnIterator(this);
 }
 
 var RowIterator = function(df) {
@@ -34,107 +271,13 @@ RowIterator.prototype["@@iterator"] = function() {
   return this;
 }
 
-DataFrame.prototype.setColumnNames = function(rowData) {
-  this.columnNames = Object.keys(rowData);
-}
-
-DataFrame.prototype["@@iterator"] = function() {
-  return new RowIterator(this);
-}
-
-DataFrame.prototype.setColumnTypes = function(rowData) {
-  var columnTypes = [];
-  var name, value, valueType;
-  for (var i = 0; i < this.columnNames.length; i++) {
-    name = this.columnNames[i];
-    value = rowData[name];
-    valueType = typeof value;
-    columnTypes.push(valueType);
-  }
-  this.columnTypes = columnTypes;
-}
-
-DataFrame.prototype.setColumns = function() {
-  var columns = [];
-  var name, type;
-  for (var i = 0; i < this.columnNames.length; i++) {
-    name = this.columnNames[i];
-    type = this.columnTypes[i];
-    var column = new Column(this, name, type, i);
-    columns[name] = column;
-    columns.push(column);
-  }
-  this.columns = columns;
-}
-
-DataFrame.prototype.row = function(rowIndex) {
-  return this.rowArray[rowIndex];
-}
-
-DataFrame.prototype.columnArray = function(colIdentifier) {
-  var columnArray = [];
-  var colName = typeof colIdentifier === "number" ? this.columnNames[colIdentifier] : colIdentifier;
-  for (var i = 0; i < this.rowArray.length; i++) {
-    columnArray.push(this.rowArray[colName]);
-  }
-  return columnArray;
-}
-
-DataFrame.prototype.slice = function(startIndex, endIndex) {
-  return new DataFrame(this.rowArray.slice(startIndex, endIndex));
-}
-
-DataFrame.prototype.row = function(rowIndex) {
-  return this.rowArray[rowIndex];
-}
-
-DataFrame.prototype.cell = function(rowIndex, colIndex) {
-  var row = this.rowArray[rowIndex];
-  return typeof row !== "undefined" ? row.get(colIndex) : undefined;
-}
-
-DataFrame.prototype.show = function() {
-  console.log(this.columnNames.join(" "));
-  for (var rowIndex = 0; rowIndex < this.rowArray.length; rowIndex++) {
-    console.log(this.rowArray[rowIndex].toString());
-  }
-}
-
-function Column(df, name, type, index) {
-  this.df = df;
-  this.name = name;
-  this.type = type;
-  this.index = index;
-}
-
-Column.prototype.get = function(rowIndex) {
-  var row = this.df.row(rowIndex)
-  return row[this.name];
-}
-
-Column.prototype.toArray = function() {
-  var columnArray = [];
-  var rowIterator = this.df["@@iterator"]();
-  var next = rowIterator.next();
-  while (!next.done) {
-    var row = next.value;
-    columnArray.push(row[this.name]);
-    next = rowIterator.next();
-  }
-  return columnArray;
-}
-
-Column.prototype["@@iterator"] = function() {
-  return new ColumnIterator;
-}
-
-var ColumnIterator = function(column) {
+var SingleColumnIterator = function(column) {
   var df = column.df;
   this.columnName = column.name;
   this.rowIterator = df["@@iterator"]();
 }
 
-ColumnIterator.prototype.next = function() {
+SingleColumnIterator.prototype.next = function() {
   var nextRowValue = this.rowIterator.next();
   if (nextRowValue.done) {
     return {done: true};
@@ -144,18 +287,34 @@ ColumnIterator.prototype.next = function() {
   }
 }
 
-function Row(df, rowData) {
-  this.df = df;
-  var columnNames = df.columnNames;
-  var columnTypes = df.columnTypes;
-  for (var i = 0; i < columnNames.length; i++) {
-    var name = columnNames[i];
-    var type = columnTypes[i];
-    var value = rowData[name];
-    if (typeof value === type) {
-      this[name] = value;
-    } else {
-      this[name] = null;
+var Row = function(rowData, schema, changedNames) {
+  this.schema = schema;
+  var columnNames = schema.columnNames;
+  var columnTypes = schema.columnTypes;
+  var name, type, value;
+  if (changedNames) {
+    var changed;
+    for (var i = 0; i < columnNames.length; i++) {
+      name = columnNames[i];
+      type = columnTypes[i];
+      changed = changedNames[name];
+      value = changed ? rowData[changed] : rowData[name];
+      if (typeof value === type) {
+        this[name] = value;
+      } else {
+        this[name] = null;
+      }
+    }
+  } else {
+    for (var i = 0; i < columnNames.length; i++) {
+      var name = columnNames[i];
+      var type = columnTypes[i];
+      var value = rowData[name];
+      if (typeof value === type) {
+        this[name] = value;
+      } else {
+        this[name] = null;
+      }
     }
   }
 }
@@ -165,12 +324,13 @@ Row.prototype.get = function(colIdentifier) {
 }
 
 Row.prototype.getByIndex = function(colIndex) {
-  var name = this.df.columnNames[colIndex];
+  var schema = this.schema;
+  var name = schema.columnNames[colIndex];
   return this[name];
 }
 
-Row.prototype.toString = function() {
-  var columnNames = this.df.columnNames;
+Row.prototype.toString = function(columns) {
+  var columnNames = columns || this.schema.columnNames;
   var rowString = "";
   for (var i = 0; i < columnNames.length; i++) {
     var name = columnNames[i];
@@ -179,8 +339,25 @@ Row.prototype.toString = function() {
   return rowString;
 }
 
+Row.prototype.toUniqueString = function(columns) {
+  var columnNames = columns || this.schema.columnNames;
+  var rowString = "";
+  for (var i = 0; i < columnNames.length; i++) {
+    var name = columnNames[i];
+    rowString += this[name] + "|";
+  }
+  return rowString;
+}
+
 module.exports = DataFrame;
 
+/*  var DataFrameRow = function(data) {
+    Row.call(this, data);
+  }
+  DataFrameRow.prototype = Object.create(Row.prototype);
+  DataFrameRow.prototype.columnNames = this.columnNames;
+  DataFrameRow.prototype.columnTypes = this.columnTypes;
+*/
 
 
 
--- test.js
+++ test.js
@@ -24,11 +24,33 @@ var data = [
 		"death_year": 1826,
 		"inauguration_year": 1801,
 		"last_year": 1809
+	},
+	{
+		"first_name": "Thomas", 
+		"last_name": "Jefferson",
+		"birth_year": 1743,
+		"death_year": 1826,
+		"inauguration_year": 1801,
+		"last_year": 1809
 	}
 ];
 
 var df = new DataFrame(data);
+//df.show();
+//console.log(df.cell(2, 4));
+//console.log(df.row(2).toString());
+//console.log(df.columns.inauguration_year.toArray());
+df = df
+  .mutate("years_in_office", function(row) { return row.last_year - row.inauguration_year; })
+  .select("first_name", "last_name", "years_in_office", {"years_in_office": "years"})
+  .rename({"first_name": "first", "last_name": "last"})
+  .distinct("first");
 df.show();
-console.log(df.cell(2, 4));
-console.log(df.row(2).toString());
-console.log(df.columns.inauguration_year.toArray());
+console.log(" ")
+var promise = df.collect();
+promise.then(function(newDf) {
+  newDf.show();
+}, function(error) {
+  throw error;
+});
+
