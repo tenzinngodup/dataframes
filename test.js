3d7e01b410e473db4c7c1ee17535499c7f59f0f4
Dan Robinson <danrobinson010@gmail.com>
2016-01-04 12:12:57 +0000

    ready to set up actual dataframes

--- test.js
+++ test.js
@@ -48,6 +48,8 @@ var data = [
 var top = new Topology(data);
 
 top.addGroupBy("party");
+top.addMutation("full_name", (row) => row.first_name + " " + row.last_name);
+top.addTap((row) => console.log(row));
 top.addSummarize("total_pres_number", (row) => sum(row.pres_number));
 top.addTap((row) => console.log(row));
 top.addSummarize("real_total", (row) => sum(row.total_pres_number));
